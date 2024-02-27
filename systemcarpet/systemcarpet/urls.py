918003c96f46cc730c678fc2b8d3e46290256208
JuanFelipe16 <javavillabon@soy.sena.edu.co>
2024-02-27 16:54:13 -0500

    time zone in settings

--- systemcarpet/systemcarpet/urls.py
+++ systemcarpet/systemcarpet/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('cart.html', views.cart, name='cart'),
     path('login.html', views.login, name='login'),
