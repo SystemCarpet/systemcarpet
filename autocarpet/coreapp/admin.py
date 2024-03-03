cb00a42cbe951a5abaa48314033e64fbc33dc8c0
tbrzc-sena <javavillabon@soy.sena.edu.co>
2024-03-03 09:20:26 -0500

    Register all models

--- autocarpet/coreapp/admin.py
+++ autocarpet/coreapp/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
-
-from .models import Usuarios,Direcciones,Barrios,Localidades
-
-admin.site.register(Direcciones)
+from .models import *
+admin.site.register(Articulos)
 admin.site.register(Barrios)
+admin.site.register(Categorias)
+admin.site.register(Colores)
+admin.site.register(DetallePedido)
+admin.site.register(Direcciones)
+admin.site.register(EntregaMateriales)
+admin.site.register(EntregasArticulos)
+admin.site.register(EstadoPedido)
+admin.site.register(EstadosArticulos)
+admin.site.register(Estampados)
+admin.site.register(MarcasVehiculos)
+admin.site.register(MaterialesOrdenPersonalizada)
+admin.site.register(MetodoEntrega)
+admin.site.register(MetodoPago)
+admin.site.register(OrdenPersonalizada)
+admin.site.register(ModelosVehiculos)
+admin.site.register(Pedidos)
 admin.site.register(Localidades)
-
+admin.site.register(PreciosProveedor)
+admin.site.register(Productos)
+admin.site.register(Proveedores)
+admin.site.register(TiposArticulos)
+admin.site.register(TiposVehiculos)
 
 
 @admin.register(Usuarios)
